64b3fc83020944cead482408fa97d5bd9f20a126
kate.koppel <katie.spb@gmail.com>
2018-08-08 13:08:04 +0300

    finished handlebars

--- spacebook/main.js
+++ spacebook/main.js
@@ -1,39 +1,39 @@
 var SpacebookApp = function () {
-  var posts = [
-    {text: "AAAAAAAA", id: 0, statusOpen: -1, comments:[
+  var postsData ={posts: [
+    {text: "AAAAAAAA", id: 0, statusOpen: false, comments:[
       { text: "A comment 1", id:0},
       { text: "A comment 2", id:1},
       { text: "A comment 3", id:2}
     ]},
-    {text: "BBBBBBB", id: 1, statusOpen: -1, comments:[
+    {text: "BBBBBBB", id: 1, statusOpen: false, comments:[
       { text: "B comment 1", id:3},
       { text: "B comment 2", id: 4},
       { text: "B comment 3", id: 5}
     ]},
-    {text: "CCCCCC", id: 2, statusOpen: -1, comments:[
+    {text: "CCCCCC", id: 2, statusOpen: false, comments:[
       { text: "c comment 1", id: 6},
       { text: "c comment 2", id: 7},
       { text: "c comment 3", id: 8}
     ]}
-  ];
+  ]};
 
   var currentId = 3;
   var currentCommentId = 9;
   var $posts = $('.posts');
 
   var _findPostById = function(id) {
-    for (var i = 0; i < posts.length; i += 1) {
-      if (posts[i].id === id) {
-        return posts[i];
+    for (var i = 0; i < postsData.posts.length; i++) {
+      if (postsData.posts[i].id === id) {
+        return postsData.posts[i];
       }
     }
   }
 
   var _deleteCommentById = function(id) {
-    for (var i = 0; i < posts.length; i += 1) {
-      for (var j =0; j < posts[i].comments.length; j++) {
-        if (posts[i].comments[j].id === id) {
-          posts[i].comments.splice(j, 1);
+    for (var i = 0; i < postsData.posts.length; i ++) {
+      for (var j =0; j < postsData.posts[i].comments.length; j++) {
+        if (postsData.posts[i].comments[j].id === id) {
+          postsData.posts[i].comments.splice(j, 1);
           return;
         }
       }
@@ -44,76 +44,38 @@ var SpacebookApp = function () {
     var $clickedPost = $(currentPost).closest('.post');
     var id = $clickedPost.data().id;
     var post = _findPostById(id);
-    return posts.indexOf(post);
+    return postsData.posts.indexOf(post);
   }
 
   var createPost = function(text) {
     var post = {
       text: text,
       id: currentId,
-      statusOpen: -1,
+      statusOpen: false,
       comments:[]
     }
     currentId += 1;
-    posts.push(post);
+    postsData.posts.push(post);
   }
 
   var createComment = function(currentPost, text) {
     var index = _getIndexOfCurrentPost(currentPost);
-    posts[index].comments.push({text:text,id:currentCommentId});
+    postsData.posts[index].comments.push({text:text,id:currentCommentId});
     currentCommentId++;
   }
 
   var renderPosts = function() {
     $posts.empty();
-
-    for (var i = 0; i < posts.length; i += 1) {
-      var post = posts[i];
-      var commentsShow = '<div class="comments-container">';
-      // if the comments were previously open for that post
-      if (post.statusOpen == 1) {
-        commentsShow = '<div class="comments-container show">';
-      }
-
-      var commentsContainer = commentsShow +
-      '<input type="text" class="comment-name">' +
-      '<button class="btn btn-primary add-comment">Post Comment</button>' +
-      '<div class="comm"></div>' + _getCommentsHTML(post)+'</div>';
-
-      $posts.append('<br><div class="post" data-id=' + post.id + '>'
-        + '<a href="#" class="remove">remove</a> ' + 
-        '<a href="#" class="show-comments">comments</a> ' +
-        post.text + commentsContainer + '</div>');
-    }
+    var source = $('#spacebook-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template(postsData);
+    $('.posts').append(newHTML);
+    
   }
-  
-  var _getCommentsHTML = function(post) {
-    var str = "";
-
-    for (let element of post.comments) {
-        str += '<div class="datacomm" data-id=' + element.id + '>'
-        + element.text + ' <a href="#" class="removeComment">Remove</a>' + '</div>';
-    }
-
-    return str;
-  }
-  /*dont use anymore
-  var renderComments = function(currentPost) {
-    var $clickedPost = $(currentPost).closest('.post');
-    $(currentPost).closest('.post').find('.comm').empty();
-    var id = $clickedPost.data().id;
-    var post = _findPostById(id);
-    var index = posts.indexOf(post);
-
-    for (let element of posts[index].comments) {
-      $clickedPost.find('.comm').append('<div class="datacomm" data-id=' + element.id + '>'
-       + element.text + ' <a href="#" class="removeComment">Remove</a>' + '</div');
-    }
-  }*/
 
   var removePost = function(currentPost) {
     var index = _getIndexOfCurrentPost(currentPost);
-    posts.splice(index, 1);
+    postsData.posts.splice(index, 1);
   }
 
   var removeComment = function(currentComment) {
@@ -125,7 +87,7 @@ var SpacebookApp = function () {
   var toggleComments = function(currentPost) {
     var $clickedPost = $(currentPost).closest('.post');
     var index = _getIndexOfCurrentPost(currentPost);
-    posts[index].statusOpen *= -1;
+    postsData.posts[index].statusOpen ^= true;
     $clickedPost.find('.comments-container').toggleClass('show');
   }
 
